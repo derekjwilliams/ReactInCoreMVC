f3aa5d8c7252001975fc5b77c5c04dceb1fa3720
Derek Williams <derek61@gmail.com>
2019-02-17 17:10:11 -0700

    initial commit

---
+++ ReactInCoreMVC/Scripts/Home/react/index.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+import * as d3 from 'd3';
+import Line from './Line.js';
+import PieWithLabels from './PieWithLabels.js';
+const App = () => (
+    <>
+        <Line></Line>
+        <PieWithLabels></PieWithLabels>
+    </>
+);
+
+render(<App />, document.getElementById('app'));
+
---
+++ ReactInCoreMVC/wwwroot/js/hello.js
@@ -0,0 +1,15 @@
+﻿import React, { Component } from 'react'
+var e = React.createElement;
+
+class HelloWorld extends React.Component {
+    render() {
+        return e(
+            "div",
+            null,
+            "Hello World"
+        );
+    }
+}
+
+const containerElement = document.getElementById('content');
+ReactDOM.render(e(HelloWorld), containerElement);
